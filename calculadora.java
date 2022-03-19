be5abf5c3df7ee8e7d804eb684e255c016cd78f8
rblancolll <rblancol@miumg.edu.gt>
2022-03-19 10:24:08 -0600

    Suma

---
+++ calculadora.java
@@ -0,0 +1,28 @@
+
+package com.mycompany.calculadoraa;
+
+
+public class calculadora {
+    //atributos
+    private int op1;
+    private int op2;
+
+    public calculadora(){
+    
+    }
+
+    public calculadora(int op1, int op2){
+        this.op1 = op1;
+        this.op2 = op2;
+
+    }
+
+    //Metodos
+     public int sumar(){
+       int suma = this.op1 + this.op2;
+       return suma;   
+     }
+    
+    
+    
+}
